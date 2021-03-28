ffe288a0e95143238cfa5955bf9e8e2eda5009a4
waluschko <waluschko@gmail.com>
2021-03-28 20:58:43 +0300

    add vrbox remote

--- VRbox/Read_events.py
+++ VRbox/Read_events.py
@@ -1,4 +1,7 @@
 import evdev
+from gpiozero import LED
+
+white=LED(17)
 
 device = evdev.InputDevice('/dev/input/event4')
 print(device)
@@ -6,4 +9,8 @@ print(device)
 for event in device.read_loop():
     if event.type == evdev.ecodes.EV_KEY:
         print(evdev.categorize(event))
-        #print(event)
+        print(event.code)
+        if event.code==115:
+            white.on()
+        else:
+            white.off()
